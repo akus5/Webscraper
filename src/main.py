bd31ccef3c6b47ed99e12e02b8e49e500d3071de
akus5 <ania.kuszynska@gmail.com>
2016-05-03 18:21:54 +0200

    collecting data

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import requests
 from selenium import webdriver
 import time
 from random import randint
+import re
+import itertools
 
 urls = []
 
@@ -29,7 +31,7 @@ class WebScraper:
 
 def get_href(page_source):
     list1 = []
-    for url in page_source.find_all("a", {"foo": "f"}):
+    for url in page_source.find_all("table", {"id": "sport_content_soccer_all"})[0].find_all("a", {"foo": "f"}):
         if str(url.get('href')).startswith('/soccer/'):
             list1.append('http://www.oddsportal.com' + url.get('href'))
     return list1
@@ -43,19 +45,29 @@ def get_href2(page_source):
     return list2[3:]
 
 
-def get_data(page_source):
-    return page_source.h1.string, page_source.select("p[class^=date]")
+def get_data1x2(page_source):
+    lst1 = {}
+    for bet in page_source.tbody:
+        try:
+            name, value = bet.get_text().rsplit(None, 1)
+        except:
+            break
+        lst1[name] = re.findall('....', value)
+    return page_source.h1.string, page_source.select("p[class^=date]")[0].string, lst1
 
 
 with WebScraper() as scraper:
     links = get_href(scraper.get_source_code('http://www.oddsportal.com/events/'))
+
     for x in links:
         try:
             urls.append(get_href2(scraper.get_source_code(x)))
         except:
             time.sleep(randint(0, 9))
-    for link in urls:
-        print(get_data(scraper.get_source_code(link)))
+
+    for link in list(itertools.chain.from_iterable(urls)):
+            print(get_data1x2(scraper.get_source_code(link)))
+
 
 
 
