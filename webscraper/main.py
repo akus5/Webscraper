c5d8d14f3472893892bc6e24c64351c56b37c6e8
akus5 <ania.kuszynska@gmail.com>
2016-05-18 13:48:37 +0200

    all bets info

--- webscraper/main.py
+++ webscraper/main.py
@@ -8,6 +8,13 @@ import itertools
 
 urls = []
 bet_1x2 = {'full_time': '#1X2;2', 'first_half': '#1X2;3', 'second_half': '#1X2;4'}
+bet_dnb = {'full_time': '#dnb;2', 'first_half': '#dnb;3', 'second_half': '#dnb;4'}
+bet_double = {'full_time': '#double;2', 'first_half': '#double;3', 'second_half': '#double;4'}
+bet_toqualify = {'FT_including_OT': '#qualify;1'}
+bet_oddoreven = {'full_time': '#odd-even;2', 'first_half': '#odd-even;3', 'second_half': '#odd-even;4'}
+bet_bts = {'full_time': '#bts;2', 'first_half': '#bts;3', 'second_half': '#bts;4'}
+bets = {'1X2': bet_1x2, 'DNB': bet_dnb, 'DC': bet_double, 'TQ': bet_toqualify, 'Odd-Even': bet_oddoreven, 'BTS': bet_bts}
+
 
 class WebScraper:
 
@@ -26,59 +33,73 @@ class WebScraper:
 
     def get_plain_source_code(self, url):
         self.driver.get(url)
+        self.driver.find_element_by_tag_name('body').send_keys("\uE035")
+        self.driver.get(url)
         return self.driver.page_source
 
 
-def get_href(page_source):
-    list1 = []
-    for url in page_source.find_all("table", {"id": "sport_content_soccer_all"})[0].find_all("a", {"foo": "f"}):
-        if str(url.get('href')).startswith('/soccer/'):
-            list1.append('http://www.oddsportal.com' + url.get('href'))
-    return list1
+class GetData:
+
+    def get_href(self, page_source):
+        list1 = []
+        for url in page_source.find_all("table", {"id": "sport_content_soccer_all"})[0].find_all("a", {"foo": "f"}):
+            if str(url.get('href')).startswith('/soccer/'):
+                list1.append('http://www.oddsportal.com' + url.get('href'))
+        return list1
+
+    def get_href2(self, page_source):
+        list2 = []
+        for url in page_source.tbody.find_all("a"):
+            if str(url.get('href')).startswith('/soccer/'):
+                list2.append('http://www.oddsportal.com' + url.get('href'))
+        return list2[3:]
+
+    def get_data1x2(self, page_source):
+        lst1 = {}
+        for bet in page_source.tbody:
+            try:
+                name, value = bet.get_text().rsplit(None, 1)
+            except:
+                break
+            lst1[name[1:]] = re.findall('....', value)
+        return page_source.h1.string, page_source.select("p[class^=date]")[0].string, lst1
+
+    def get_data(self, link):
+        for key, bet in bets.items():
+            for key1, rest in bet.items():
+                with WebScraper() as scraper1:
+                    print(key + " " + key1)
+                    print(self.get_data1x2(scraper1.get_source_code(link + rest)))
 
 
-def get_href2(page_source):
-    list2 = []
-    for url in page_source.tbody.find_all("a"):
-        if str(url.get('href')).startswith('/soccer/'):
-            list2.append('http://www.oddsportal.com' + url.get('href'))
-    return list2[3:]
 
 
-def get_data1x2(page_source):
-    lst1 = {}
-    for bet in page_source.tbody:
-        try:
-            name, value = bet.get_text().rsplit(None, 1)
-        except:
-            break
-        lst1[name[1:]] = re.findall('....', value)
-    return page_source.h1.string, page_source.select("p[class^=date]")[0].string, lst1
 
+get = GetData()
 
 with WebScraper() as scraper:
-    links = get_href(scraper.get_source_code('http://www.oddsportal.com/events/'))
-
+    links = get.get_href(scraper.get_source_code('http://www.oddsportal.com/events/'))
+    print(links)
     for x in links:
         try:
-            ln = get_href2(scraper.get_source_code(x))
+            ln = get.get_href2(scraper.get_source_code(x))
             # urls.append(ln)
             print("Pobrano linki z :" + x)
         except:
             time.sleep(randint(0, 9))
         for link in ln:
             try:
-                print(get_data1x2(scraper.get_source_code(link)))
+                print(get.get_data(link))
             except:
                 time.sleep(randint(0, 9))
 
-    '''
-    for link in list(itertools.chain.from_iterable(urls)):
-        try:
-            print(get_data1x2(scraper.get_source_code(link)))
-        except:
-            time.sleep(randint(0, 9))
-    '''
+'''
+for link in list(itertools.chain.from_iterable(urls)):
+    try:
+        print(get_data1x2(scraper.get_source_code(link)))
+    except:
+        time.sleep(randint(0, 9))
+'''
 
 
 
